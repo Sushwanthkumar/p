f79e844d5d23876e145e65beb22ad0fb4880aef0
sushwanth <sushwanth.cse2024@gmail.com>
2024-08-11 11:11:35 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+       <header>
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Skills</a></li>
+                <li><a href="https://drive.google.com/file/d/1lzVnuKc9n6ypMaYu-sV9tySC3Z2lU72f/view?usp=drivesdk">Certificates</a></li>
+                <li><a href="tel:9381502239">Call us at 938-150-2239</a></li>
+        </ul>
+    </nav>
+   </header>
+   <main>
+    <section class="in">
+        <div class="int">
+            <h2>Hello, It's Me</h2>
+            <h3>VAVILETI SUSHWANTH KUMAR</h3>
+            <h4>And I'm a <span>FULL STACK DEVELOPER</span></h4>-
+            <div class="into">
+                <a href="https://wa.me/qr/BUJF7D5EJGHQK1"><img src="what.png"></img> </a>
+                <a href="https://www.instagram.com/sushwanth_18?igsh=MTFjanlsN2lod3praA=="><img src="insta.png"></img></a>
+                <a href="https://www.linkedin.com/in/vavileti-sushwanth-kumar-964039199"><img src="link.png"></img></a>
+            </div>
+            <a href="#" class="btn">Download CV</a>
+        </div>
+        <div class="img">
+            <img src="sushwanth.png" ></img> 
+        </div>
+    </section>
+</main>
+
+
+         
+    </div>
+  );
+}
+
+export default App;
